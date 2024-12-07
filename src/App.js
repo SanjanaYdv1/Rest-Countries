f7ca7866fe2b7852b9b8cead8e7062297bcdaac4
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2024-12-07 18:05:56 +0545

    added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Home />
+      
+    </div>
+  );
+}
+
+export default App;
